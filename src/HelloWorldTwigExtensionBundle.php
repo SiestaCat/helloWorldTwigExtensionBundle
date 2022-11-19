0bd467ea62a901a8be08544b32268688345af8a3
Marco <marco.isfc@gmail.com>
2022-11-19 05:56:49 +0100

    Fix bundle

--- src/HelloWorldTwigExtensionBundle.php
+++ src/HelloWorldTwigExtensionBundle.php
@@ -2,23 +2,20 @@
 
 namespace SiestaCat\HelloWorldTwigExtensionBundle;
 
-
+use SiestaCat\HelloWorldTwigExtensionBundle\Twig\HelloWorldExtension;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 /**
  * HelloWorldTwigExtensionBundle
  */
 class HelloWorldTwigExtensionBundle extends AbstractBundle
 {
-    public function load(array $configs, ContainerBuilder $container)
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
-        $loader = new YamlFileLoader(
-            $container,
-            new FileLocator(__DIR__.'/../config')
-        );
-        $loader->load('services.yaml');
+        $container->services()->set('helloworldtwigextensionbundle', HelloWorldExtension::class);
     }
 }
