c53027871ce1c38f511659013bddc8468048efa4
Marco <marco.isfc@gmail.com>
2022-11-19 06:13:31 +0100

    fix service auto load

--- src/HelloWorldTwigExtensionBundle.php
+++ src/HelloWorldTwigExtensionBundle.php
@@ -8,14 +8,16 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
+use Acme\HelloBundle\DependencyInjection\UnconventionalExtensionClass;
 
 /**
  * HelloWorldTwigExtensionBundle
  */
-class HelloWorldTwigExtensionBundle extends AbstractBundle
+final class HelloWorldTwigExtensionBundle extends AbstractBundle
 {
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
-        $container->services()->set('helloworldtwigextensionbundle', HelloWorldExtension::class);
+        $container->import(__DIR__ . '/../config/services.xml');
     }
 }
